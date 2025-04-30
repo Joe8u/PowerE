68f5a742f5c32d86500427ab2022274d82029994
Joe8u <82503508+Joe8u@users.noreply.github.com>
2025-04-30 19:15:41 +0200

    Refactor

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,39 +1,18 @@
 # src/dashboard/app.py
-import dash
-import dash_bootstrap_components as dbc
-from dash import html, dcc
-from dash.dependencies import Input, Output
-
-# Layout imports (only layouts and callback registration functions)
-from dashboard.pages.summary   import layout as summary_layout,   register_callbacks as register_summary
-from dashboard.pages.details   import layout as details_layout,   register_callbacks as register_details
+from dash import Dash, html
 from dashboard.pages.scenarios import layout as scenarios_layout, register_callbacks as register_scenarios
 
-# Create Dash app
-app = dash.Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    suppress_callback_exceptions=True
-)
-server = app.server
+# Initialisiere die Dash-App
+app = Dash(__name__, title="PowerE-Dash")
 
-# Main layout with URL routing
+# Definiere das Hauptlayout (vorläufig nur mit der Scenarios-Seite)
 app.layout = html.Div([
-    dcc.Location(id="url", refresh=False),
-    html.Div(id="page-content")
+    html.H1("PowerE-Dash: Haushaltslastverschiebung"),
+    scenarios_layout  # Integriere das Layout der Scenarios-Seite
 ])
 
-# Page switcher
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-def display_page(pathname):
-    if pathname == "/details":
-        return details_layout
-    elif pathname == "/scenarios":
-        return scenarios_layout
-    else:
-        return summary_layout
-
-# Register callbacks after app and server are defined
-register_summary(app)
-register_details(app)
+# Registriere Callbacks für die Scenarios-Seite
 register_scenarios(app)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
