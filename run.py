590eed074603f55792b567f995cbe0d4e0cc83f7
Joe8u <82503508+Joe8u@users.noreply.github.com>
2025-05-01 12:11:27 +0200

    Add run.py to fix ImportError

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 from dashboard.app import app
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
